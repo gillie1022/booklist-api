c4da0e4fdacca264ba160b07b88bdb831f145ddc
gillie1022 <dave.gillie@icloud.com>
2020-06-27 16:01:04 -0400

    migrated models

--- api/models.py
+++ api/models.py
@@ -9,6 +9,15 @@ class Author(models.Model):
     author = models.CharField(max_length=100, unique=True)
 
 class Book(models.Model):
-    user = models.ForeignKey(to=User, on_delete=CASCADE, related_name='books')
-    title = models.CharField(max_length=500)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='books', null=True, blank=True)
+    title = models.CharField(max_length=500, null=True, blank=True)
     authors = models.ManyToManyField(to=Author, related_name='books')
+    TO_READ = 'to read'
+    READING = 'reading'
+    READ = 'read'
+    STATUS_CHOICES = [
+        (TO_READ, 'to read'),
+        (READING, 'reading'),
+        (READ, 'read')
+    ]
+    status = models.CharField(max_length=7, choices=STATUS_CHOICES, default=TO_READ)
