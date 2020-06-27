569e011324237173a044064f12b7f35d2712ea10
gillie1022 <dave.gillie@icloud.com>
2020-06-27 15:33:51 -0400

    made some models, need to do some research on choicefields

--- api/models.py
+++ api/models.py
@@ -5,5 +5,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
+class Author(models.Model):
+    author = models.CharField(max_length=100, unique=True)
+
 class Book(models.Model):
-    pass
+    user = models.ForeignKey(to=User, on_delete=CASCADE, related_name='books')
+    title = models.CharField(max_length=500)
+    authors = models.ManyToManyField(to=Author, related_name='books')
